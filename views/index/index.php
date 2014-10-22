d6ec906963f3eb2d5a3cc1f8e36bd3741fa1fbb2
jysong14 <song14@illinois.edu>
2014-10-22 00:33:40 -0500

    1.Index & Signup

--- views/index/index.php
+++ views/index/index.php
@@ -1,3 +1,32 @@
-<div>
-    Hello world!
-</div>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>COMJOB</title>
+	</head>
+	<body>
+		<section id = "titleBox">
+			<h1>Let us find you a COMJOB</h1>
+		</section>
+		<section id="loginBox">
+			<h2>Login</h2>
+			<form method="post" class="minimal" action="">
+				<label for="username">
+					Username:
+					<input type="text" name="username" id="username" required="required" />
+				</label>
+				<label for="password">
+					Password:
+					<input type="password" name="password" id="password" required="required" />
+				</label>
+				<button type="submit" class="btn-submit">Sign in</button>
+			</form>
+		</section>
+		<section id = "newUserBox">
+			<h3>Are you a new user?
+				<form method="post" action="signup">
+					<button type="newUser" class="btn-newUser">New User</button>
+				</form>
+			</h3>
+		</section>
+	</body>	
+</html>
--- views/signup/signup.php
+++ views/signup/signup.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>Profile Page</title>
+		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
 		<section id = "titleBox">
