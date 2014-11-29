de88867cc375f1cc528ba03f48144dc6bda963b9
Dong Min Shin <dshin9@illinois.edu>
2014-11-28 22:44:45 -0600

    merge changes

--- config/constant.php
+++ config/constant.php
@@ -4,11 +4,8 @@
  * @date    July 17, 2014
  */
 
-<<<<<<< HEAD
+
 define('URL', 'http://localhost/comjob/');
-=======
-define('URL', 'http://localhost:8888/comjob/cs411_project/');
->>>>>>> 530b24f8459fc693ac6e05f114c04960c3fdad36
 define('ADMIN_EMAIL', 'jameslee8611@gmail.com');
 
 // setting menu
